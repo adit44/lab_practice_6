cd57e91f86db80e5b3cdb9cae16c93d78511e8e3
aditya <msasongko17@ku.edu.tr>
2020-11-23 15:54:43 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// Name:
+// Date:
+
+// Answers the Questions 1 and Question 4 here:
+
+// Question 1 a) 
+// Question 1 b) 
+// Question 4) 
+
+
+/*
+ * someFunction (Task 2)
+ * Check for arguments of this function in the manual (all args are int)
+ * Read and understand the manual for instructions.
+ * returns an integer
+ */
+int someFunction( ***** ) {
+    
+}
+
+
+/*
+ * distsq (Task 3)
+ * Check for arguments of this function in the manual(all args are int)
+ * Read and understand the manual for instructions.
+ * returns an integer
+ */
+int distsq( ***** ) {
+
+}
+
+
+
+int main() 
+{
+	if ((someFunction(6) == 59) && (someFunction(10) == 123)){
+		printf("someFunction test passed!\n");
+	}
+	else{
+		printf("someFunction test failed!\n");
+	}
+
+  if ((distsq(8, 6) == 100) && (distsq(3,4) == 25)){
+		printf("distsq test passed!\n");
+	}
+	else{
+		printf("distsq test failed!\n");
+	}
+	return 0;
+}
